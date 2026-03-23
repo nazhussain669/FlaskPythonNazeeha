5186f10bbd6fdb1806d91af21fca5fc81fe4c627
nazhussain669 <nazeehah@nycstudents.net>
2026-03-23 11:03:30 -0400

    Add files via upload

---
+++ displayinfo_table.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    schools = ["", "Hillcrest High School"]
+
+    programs = ["", "CTE", "Non CTE"]
+
+    paths = ["", "CAD - Computer Aided-Design",
+             "CNA - Certified Nursing Assisting",
+             "EMS - Emergency Medical Services",
+             "SWE - Software Engineering",
+             "VE - Virtual Enterprise/Entrepreneur"]
+
+    return render_template('displayinfo_table1.html',
+                           schools=schools,
+                           programs=programs,
+                           paths=paths)
+
+@app.route('/output')
+def CollectInfo():
+
+    fname = request.args.get("fname")
+    mname = request.args.get("mname")
+    lname = request.args.get("lname")
+    bday = request.args.get("bday")
+    id = request.args.get("id")
+    school = request.args.get("school")
+    program = request.args.get("program")
+    path = request.args.get("path")
+
+    if mname:
+        midname = mname + "."
+    else:
+        midname = "NONE"
+
+    student = {
+        "id": id,
+        "firstname": fname,
+        "midinit": midname,
+        "lastname": lname,
+        "bday": bday,
+        "school": school,
+        "program": program,
+        "path": path
+    }
+
+    return render_template("displayinfo_table2.html",
+                           student=student)
+
+if __name__ == "__main__":
+    app.run()
